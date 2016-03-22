4661cf54a5986862e609dded113ef7bb666d2090
Christian <c.brueggemann94@gmail.com>
2016-03-22 22:13:04 +0100

    Initial commit

---
+++ src/main/java/de/cbruegg/ormliterx/OrmLiteRx.java
@@ -0,0 +1,63 @@
+package de.cbruegg.ormliterx;
+
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.stmt.Where;
+import rx.Observable;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+/**
+ * Collection of thing wrappers around ORMLite
+ * for usage with RxJava.
+ */
+public final class OrmLiteRx {
+
+    private OrmLiteRx() {
+    }
+
+    /**
+     * @see Where#query()
+     */
+    public static <T, ID> Observable<List<T>> query(final Where<T, ID> where) {
+        return Observable.fromCallable(new Callable<List<T>>() {
+            public List<T> call() throws Exception {
+                return where.query();
+            }
+        });
+    }
+
+    /**
+     * @see QueryBuilder#query()
+     */
+    public static <T, ID> Observable<List<T>> query(final QueryBuilder<T, ID> qb) {
+        return Observable.fromCallable(new Callable<List<T>>() {
+            public List<T> call() throws Exception {
+                return qb.query();
+            }
+        });
+    }
+
+    /**
+     * @see Where#queryForFirst()
+     */
+    public static <T, ID> Observable<T> queryForFirst(final Where<T, ID> where) {
+        return Observable.fromCallable(new Callable<T>() {
+            public T call() throws Exception {
+                return where.queryForFirst();
+            }
+        });
+    }
+
+    /**
+     * @see QueryBuilder#queryForFirst()
+     */
+    public static <T, ID> Observable<T> queryForFirst(final QueryBuilder<T, ID> qb) {
+        return Observable.fromCallable(new Callable<T>() {
+            public T call() throws Exception {
+                return qb.queryForFirst();
+            }
+        });
+    }
+
+}
