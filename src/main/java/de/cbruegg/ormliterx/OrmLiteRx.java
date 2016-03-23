2270d880a45809f83cca0d628063f34ef2cd55c9
Christian <c.brueggemann94@gmail.com>
2016-03-23 13:04:14 +0100

    Add support for new RuntimeException variants

--- src/main/java/de/cbruegg/ormliterx/OrmLiteRx.java
+++ src/main/java/de/cbruegg/ormliterx/OrmLiteRx.java
@@ -1,6 +1,10 @@
 package de.cbruegg.ormliterx;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
 import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.stmt.RuntimeExceptionQueryBuilder;
+import com.j256.ormlite.stmt.RuntimeExceptionWhere;
 import com.j256.ormlite.stmt.Where;
 import rx.Observable;
 
@@ -17,9 +21,42 @@ public final class OrmLiteRx {
     }
 
     /**
+     * @see Dao#queryForAll()
+     */
+    public static <T, ID> Observable<List<T>> queryForAll(final Dao<T, ID> dao) {
+        return Observable.fromCallable(new Callable<List<T>>() {
+            public List<T> call() throws Exception {
+                return dao.queryForAll();
+            }
+        });
+    }
+
+    /**
+     * @see RuntimeExceptionDao#queryForAll()
+     */
+    public static <T, ID> Observable<List<T>> queryForAll(final RuntimeExceptionDao<T, ID> dao) {
+        return Observable.fromCallable(new Callable<List<T>>() {
+            public List<T> call() throws Exception {
+                return dao.queryForAll();
+            }
+        });
+    }
+
+    /**
      * @see Where#query()
      */
     public static <T, ID> Observable<List<T>> query(final Where<T, ID> where) {
+        return Observable.fromCallable(new Callable<List<T>>() {
+            public List<T> call() throws Exception {
+                return where.query();
+            }
+        });
+    }
+
+    /**
+     * @see RuntimeExceptionWhere#query()
+     */
+    public static <T, ID> Observable<List<T>> query(final RuntimeExceptionWhere<T, ID> where) {
         return Observable.fromCallable(new Callable<List<T>>() {
             public List<T> call() throws Exception {
                 return where.query();
@@ -39,9 +76,31 @@ public final class OrmLiteRx {
     }
 
     /**
+     * @see RuntimeExceptionQueryBuilder#query()
+     */
+    public static <T, ID> Observable<List<T>> query(final RuntimeExceptionQueryBuilder<T, ID> qb) {
+        return Observable.fromCallable(new Callable<List<T>>() {
+            public List<T> call() throws Exception {
+                return qb.query();
+            }
+        });
+    }
+
+    /**
      * @see Where#queryForFirst()
      */
     public static <T, ID> Observable<T> queryForFirst(final Where<T, ID> where) {
+        return Observable.fromCallable(new Callable<T>() {
+            public T call() throws Exception {
+                return where.queryForFirst();
+            }
+        });
+    }
+
+    /**
+     * @see RuntimeExceptionWhere#queryForFirst()
+     */
+    public static <T, ID> Observable<T> queryForFirst(final RuntimeExceptionWhere<T, ID> where) {
         return Observable.fromCallable(new Callable<T>() {
             public T call() throws Exception {
                 return where.queryForFirst();
@@ -56,6 +115,61 @@ public final class OrmLiteRx {
         return Observable.fromCallable(new Callable<T>() {
             public T call() throws Exception {
                 return qb.queryForFirst();
+            }
+        });
+    }
+
+    /**
+     * @see RuntimeExceptionQueryBuilder#queryForFirst()
+     */
+    public static <T, ID> Observable<T> queryForFirst(final RuntimeExceptionQueryBuilder<T, ID> qb) {
+        return Observable.fromCallable(new Callable<T>() {
+            public T call() throws Exception {
+                return qb.queryForFirst();
+            }
+        });
+    }
+
+    /**
+     * @see Dao#queryForId(Object)
+     */
+    public static <T, ID> Observable<T> queryForFirst(final Dao<T, ID> dao, final ID id) {
+        return Observable.fromCallable(new Callable<T>() {
+            public T call() throws Exception {
+                return dao.queryForId(id);
+            }
+        });
+    }
+
+    /**
+     * @see RuntimeExceptionDao#queryForId(Object)
+     */
+    public static <T, ID> Observable<T> queryForId(final RuntimeExceptionDao<T, ID> dao, final ID id) {
+        return Observable.fromCallable(new Callable<T>() {
+            public T call() throws Exception {
+                return dao.queryForId(id);
+            }
+        });
+    }
+
+    /**
+     * @see Dao#queryForEq(String, Object)
+     */
+    public static <T, ID> Observable<List<T>> queryForEq(final Dao<T, ID> dao, final String column, final Object o) {
+        return Observable.fromCallable(new Callable<List<T>>() {
+            public List<T> call() throws Exception {
+                return dao.queryForEq(column, o);
+            }
+        });
+    }
+
+    /**
+     * @see RuntimeExceptionDao#queryForEq(String, Object)
+     */
+    public static <T, ID> Observable<List<T>> queryForEq(final RuntimeExceptionDao<T, ID> dao, final String column, final Object o) {
+        return Observable.fromCallable(new Callable<List<T>>() {
+            public List<T> call() throws Exception {
+                return dao.queryForEq(column, o);
             }
         });
     }
